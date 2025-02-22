a377ffa3cecc6344031f084805f38bd335313f5d
chimnayajith <chinmayajith30@gmail.com>
2025-02-22 09:24:22 +0530

    Initialize project with basic structure

---
+++ bin/no_wyr.dart
@@ -0,0 +1,5 @@
+import 'package:no_wyr/no_wyr.dart' as no_wyr;
+
+void main(List<String> arguments) {
+  print('Hello world: ${no_wyr.calculate()}!');
+}
---
+++ test/no_wyr_test.dart
@@ -0,0 +1,8 @@
+import 'package:no_wyr/no_wyr.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
